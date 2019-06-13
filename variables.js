62e550436e9a9cc621235d7c5518bc4d42bf542c
Gerald <maboshegerald1@gmail.com>
2019-06-13 21:09:42 +0200

    added arithmetic changes to variables.js

--- variables.js
+++ variables.js
@@ -1,1 +1,16 @@
-// you solution should be here
+var birthYear = 1996;
+var futureYear = 2020;
+var age = futureYear - birthYear;
+var anotherAge = age - 1;
+console.log(age + " years");
+console.log(anotherAge + " years");
+
+var myCurrentAge = 22;
+var maxAge = 78;
+var estAmount = 2;
+
+numDays = (maxAge - myCurrentAge) * 365;
+var lastingAmount = numDays * 2;
+
+console.log("You will need " + lastingAmount + " amount of food to last you until the ripe old age of " + maxAge);
+
